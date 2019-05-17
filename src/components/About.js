51db9dd4cea509740b38967c775bc262d3b54e41
Swapnull <martyn.e.rushton@gmail.com>
2019-05-17 20:21:48 +0200

    init

---
+++ src/components/About.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+function HomeComponent() {
+  return <h1>About Component</h1>;
+}
+
+export default HomeComponent;
---
+++ src/components/Categories.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+function HomeComponent(props) {
+  return <h1>Categories Component</h1>;
+}
+
+export default HomeComponent;
